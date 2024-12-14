7ebae598606e2519b28eafbfe1fd85c325bca560
FxMorin <28154542+fxmorin@users.noreply.github.com>
2024-12-14 16:11:01 -0500

    Final JavaDoc pass for toggle and initializer

--- src/main/java/ca/fxco/api/pistonlib/toggle/ToggleableProperties.java
+++ src/main/java/ca/fxco/api/pistonlib/toggle/ToggleableProperties.java
@@ -3,15 +3,19 @@ package ca.fxco.api.pistonlib.toggle;
 import java.util.function.BooleanSupplier;
 
 /**
- * @author FX
+ * Toggleable for properties.
+ *
  * @param <T> object to toggle
+ * @author FX
  * @since 1.0.4
  */
 public interface ToggleableProperties<T> {
 
     /**
-     * Allows you to conditionally set if this object is disabled
+     * Allows you to conditionally set if this object is disabled.
+     *
      * @param isDisabled boolean supplier to set disabled to
+     * @return self, used in builder constructs
      * @since 1.0.4
      */
     T pl$setDisabled(BooleanSupplier isDisabled);
