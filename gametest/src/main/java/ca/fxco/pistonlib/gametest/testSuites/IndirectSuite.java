22646ac1e3a626d157cec4351ebed3344aeff68f
FxMorin <28154542+fxmorin@users.noreply.github.com>
2025-01-17 23:12:18 -0500

    Add indirect tests

--- gametest/src/main/java/ca/fxco/pistonlib/gametest/testSuites/IndirectSuite.java
+++ gametest/src/main/java/ca/fxco/pistonlib/gametest/testSuites/IndirectSuite.java
@@ -16,4 +16,36 @@ public class IndirectSuite {
     )
     @GameTest(timeoutTicks = 4)
     public void side1(GameTestHelper helper) {}
+
+    // Chests shouldn't pull beds when using indirectStickyApi. https://github.com/FxMorin/PistonLib/pull/48
+    @GameTestLib("allStickyTypesAreIndirect")
+    @GameTest(timeoutTicks = 4)
+    public void doubleblocks1(GameTestHelper helper) {}
+
+    // Chests shouldn't be pulled through blocks when using indirectStickyApi. https://github.com/FxMorin/PistonLib/pull/48
+    @GameTestLib("allStickyTypesAreIndirect")
+    @GameTest(timeoutTicks = 4)
+    public void doubleblocks2(GameTestHelper helper) {}
+
+    // Glue blocks shouldn't move stickyless blocks
+    @GameTestLib("allStickyTypesAreIndirect")
+    @GameTest(timeoutTicks = 4)
+    public void stickyless1(GameTestHelper helper) {}
+
+    // Piston shouldn't pull stickyless blocks right infront of it
+    @GameTestLib("allStickyTypesAreIndirect")
+    @GameTest(timeoutTicks = 4)
+    public void stickyless2(GameTestHelper helper) {}
+
+    // Piston shouldn't pull half_obsidian blocks right infront of it.
+    // This is different from stickyless2 to check if the direction is correct
+    @GameTestLib("allStickyTypesAreIndirect")
+    @GameTest(timeoutTicks = 4)
+    public void stickyless3(GameTestHelper helper) {}
+
+    // Check if having a slime block inbetween can change the outcome
+    @GameTestLib("allStickyTypesAreIndirect")
+    @GameTest(timeoutTicks = 4)
+    public void stickyless4(GameTestHelper helper) {}
+
 }
